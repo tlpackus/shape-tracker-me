60dbdad59dcb1a162907bd731fe2a8b858177497
tlpackus <tlpackus@gmail.com>
2021-03-06 23:29:12 -0800

    add fourth test

--- src/triangle.js
+++ src/triangle.js
@@ -2,7 +2,7 @@ export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
-}
+};
 
 Triangle.prototype.checkType = function() {
   return "not a triangle";
@@ -13,5 +13,9 @@ Triangle.prototype.checkType = function() {
     return "not a triangle";
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
     return "scalene triangle";
+  }  else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+    return "isosceles triangle";
   }
-}
+};
+
+
